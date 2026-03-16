2428d7dee00ee5147a2e69140371e2b0e3321636
Xuan Bach Trinh <txbworkspace@gmail.com>
2026-03-17 03:51:48 +0700

    hhh

--- src/components/theme/theme-provider.tsx
+++ src/components/theme/theme-provider.tsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
 
 type Theme = "light" | "dark";
+const DEFAULT_SIDEBAR = "gradientsidebar3";
+const DEFAULT_TOPBAR = "gradienttopbar3";
+// Thiết lập màu chủ đạo cho các thành phần như nút, link, checkbox...
+const DEFAULT_PRIMARY_COLOR = "info";
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
   const [theme, setTheme] = useState<Theme>(() => {
@@ -9,7 +13,17 @@ export function ThemeProvider({ children }: { children: React.ReactNode }) {
   });
 
   useEffect(() => {
-    document.documentElement.classList.toggle("dark", theme === "dark");
+    document.documentElement.setAttribute("data-bs-theme", theme);
+
+    // 2. Màu sắc Sidebar
+    document.documentElement.setAttribute("data-sidebar", DEFAULT_SIDEBAR);
+
+    // 3. Màu sắc Topbar
+    document.documentElement.setAttribute("data-topbar", DEFAULT_TOPBAR);
+
+    // 4. Màu chủ đạo hệ thống
+    document.documentElement.setAttribute("data-color", DEFAULT_PRIMARY_COLOR);
+
     localStorage.setItem("theme", theme);
   }, [theme]);
 
